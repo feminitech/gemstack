60238555731a072a8fefa4c349e1e12161571b5f
femintech <nayara@feminitech.com.br>
2020-06-01 14:52:19 -0300

    inicio automaçao API

--- api/spec/sessions_spec.rb
+++ api/spec/sessions_spec.rb
@@ -1,9 +1,19 @@
 
+require "httparty"
 
 describe "POST /sessions" do
     
     it "new session" do
 
+        payload = { email: "nayara@feminitech.com.br" }
+        
+        result = HTTParty.post(
+            "http://spotlab-api.herokuapp.com/sessions"
+            bory: payload,
+            headers: {"Content-Type": "application/json" }
+        )
+
+        puts result.response.code
     end
 
 end
